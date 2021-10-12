db725f635be613ffff444492073afe28035012f8
Keith Chiang <keithjigsaw@gmail.com>
2021-10-12 17:45:55 +0800

    [G6T8-94] Modified and added endpoints based on new tables/schema
    - Others: Installed NodeMon

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -18,7 +18,7 @@ router.get("/", (req, res) => {
 })
 
 router.get("/getCourse", (req, res) => {
-	let sql = `SELECT * from courses WHERE courseID="${req.query.courseID}"`
+	let sql = `SELECT * from classes WHERE course_id="${req.query.course_id}"`
 	db.query(sql, (err, result) => {
 		if (err) {
 			res.status(500).send({
@@ -32,9 +32,23 @@ router.get("/getCourse", (req, res) => {
     
 })
 
-router.get("/getPrerequisite", (req, res) => {
-	let sql = `SELECT prereqCourseID from course_prereq WHERE courseID="${req.query.courseID}"`
-    console.log(sql);
+
+// Retrieve a course's class information (engineer viewing a course)
+router.get("/getClassInfo", (req, res) => {
+	let sql = `
+        SELECT
+            course.course_id, course.course_name, course.course_summary, class.class_id, t.name, t.email, class.size, class.current_enrolled, class.enrolment_start, class.enrolment_end, class.course_start, class.course_end
+        FROM
+            courses course, classes class, trainers t
+        WHERE
+            course.course_id = class.course_id
+        AND
+            t.email = class.trainer_email
+        AND
+            class.course_id="${req.query.course_id}
+        AND
+            class.class_id="${req.query.class_id}
+    `
 	db.query(sql, (err, result) => {
 		if (err) {
 			res.status(500).send({
@@ -42,149 +56,221 @@ router.get("/getPrerequisite", (req, res) => {
 			})
 		} else {
 			// array of rows are already in the format of "data": []
-            console.log(result);
-			res.json(result)
+			res.json(result[0])
 		}
 	})
     
 })
 
-router.post("/createCourse", (req, res) => {
-	let sql = `INSERT INTO courses VALUES ( \
-		"${req.body.courseID}", \
-		"${req.body.courseName}", \
-		"${req.body.class}", \
-		${req.body.size}, \
-		"${req.body.trainer}", \
-		"${req.body.enrolmentStart}", \
-		"${req.body.enrolmentEnd}", \
-		"${req.body.startDate}", \
-		"${req.body.endDate}", \
-		DEFAULT, DEFAULT)`
-
+// Retrieve a SPECIFIC course's prerequisites
+router.get("/getPrereq", (req, res) => {
+	let sql = `
+        SELECT
+            prereq_course_id FROM courses c, course_prereq cpr
+        WHERE
+            c.course_id = cpr.course_id
+        AND
+            c.course_id="${req.query.course_id}
+    `
 	db.query(sql, (err, result) => {
-		if (err) {
-			res.status(500).send({
-				message: err.message || "An error has occurred.",
-				sql: sql
-			})
-		} else {
-			console.log("1 record inserted to courses table")
-		}
-	})
-})
-
-router.post("/editCourse", (req, res) => {
-	let sql = `UPDATE courses SET \
-		courseName = "${req.body.courseName}", \
-		class = "${req.body.class}", \
-		size = ${req.body.size}, \
-		trainer = "${req.body.trainer}", \
-		enrolmentStart = "${req.body.enrolmentStart}", \
-		enrolmentEnd = "${req.body.enrolmentEnd}", \
-		startDate = "${req.body.startDate}", \
-		endDate = "${req.body.endDate}"
-		WHERE courseID = "${req.body.courseID}"`
-
-	db.query(sql, (err, result) => {
-		if (err) {
-			res.status(500).send({
-				message: err.message || "An error has occurred.",
-				sql: sql
-			})
-		} else {
-			console.log("1 record edited and saved into table")
-		}
-	})
-})
-
-router.post("/publishCourse", (req, res) => {
-	let sql = `UPDATE courses SET isPublished = True \
-		WHERE courseID = "${req.body.courseID}"`
-
-	db.query(sql, (err, result) => {
-		if (err) {
-			res.status(500).send({
-				message: err.message || "An error has occurred.",
-				sql: sql
-			})
-		} else {
-			console.log(`Course ${req.body.courseID} is published.`)
-		}
-	})
-})
-
-/* Retrieve AVAILABLE courses 
-Courses WHERE there are either:
-1. no pre-requisites OR,
-2. courses that I have completed, where I meet those prerequisites BUT have not completed them */
-// ASSUMPTIONS NOW: All courses are available to be taught - not considering enrolment period yet
-router.get("/retrieveEligible", (req, res) => {
-	let sql = ` SELECT * FROM Courses
-                WHERE courseID not in (SELECT courseID FROM completed_courses WHERE email ='keithchiang.2019@aio.com') 
-                AND hasPrereq = False
-                OR (courseID in (SELECT courseID FROM course_prereq
-                                WHERE prereqCourseID in (SELECT courseID FROM completed_courses WHERE email='keithchiang.2019@aio.com')
-                                AND courseID not in (SELECT courseID FROM completed_courses WHERE email='keithchiang.2019@aio.com'))
-                    ); `;
-
-	db.query(sql, (err, rows) => {
 		if (err) {
 			res.status(500).send({
 				message: err.message || "An error has occurred."
 			})
 		} else {
 			// array of rows are already in the format of "data": []
-			res.json(rows) 
+			res.json(result[0])
 		}
 	})
+    
 })
 
-// Retrieve completed courses
-router.get("/retrieveCompleted", (req, res) => {
-	let sql = `SELECT c.courseID, c.courseName, c.trainer, cc.completedDate
-                FROM courses c, completed_courses cc
-                WHERE c.courseID = cc.courseID
-                AND email = 'keithchiang.2019@aio.com'
-                `;
-
-	db.query(sql, (err, rows) => {
+// Retrieve completed courses + course details FOR engineer='keithchiang.2019@aio.com'
+router.get("/getCompleted", (req, res) => {
+	let sql = `
+        SELECT
+            course.course_id, course.course_name, course.course_summary, class.class_id, class.size, class.course_start, class.course_end
+        FROM
+            courses course, classes class, completed_courses completed
+        WHERE
+            course.course_id = class.course_id
+        AND
+            class.course_id = completed.course_id
+        AND
+            class.class_id = completed.class_id
+        AND
+            completed.engineer_email = 'keithchiang.2019@aio.com';
+    `
+	db.query(sql, (err, result) => {
 		if (err) {
 			res.status(500).send({
 				message: err.message || "An error has occurred."
 			})
 		} else {
-			// array of rows are already in the format of "data": []
-			res.json(rows) 
+			res.json(result)
 		}
 	})
 })
 
-/*  RETRIEVE INELIGIBLE COURSES
-    Courses WHERE
-    1. there are prerequisites AND
-    2. courses that I have not met the prerequisites for AND have not completed
-*/
-router.get("/retrieveIneligible", (req, res) => {
-	let sql = ` SELECT  c.courseID, c.courseName, c.class, c.size, c.trainer, c.startDate, c.endDate, p.prereqCourseID
-                FROM courses c, course_prereq p
-                WHERE c.courseID = p.courseID
-                AND c.hasPrereq = True
-                AND c.courseID in (SELECT courseID FROM course_prereq
-                WHERE prereqCourseID not in (SELECT courseID FROM completed_courses WHERE email='keithchiang.2019@aio.com'));
-                
-                `;
+// Retrieve ongoing courses + details FOR engineer='keithchiang.2019@aio.com'
+router.get("/getOngoing", (req, res) => {
+	let sql = `
+        SELECT
+            course.course_id, course.course_name, course.course_summary, class.class_id, class.trainer_email, class.size, class.course_start, class.course_end
+        FROM
+            courses course, classes class, enrolled enroll
+        WHERE
+            course.course_id = enroll.course_id
+        AND
+            class.class_id = enroll.class_id
+        AND
+            class.course_id = enroll.course_id';
+    `
 
-	db.query(sql, (err, rows) => {
+	db.query(sql, (err, result) => {
 		if (err) {
 			res.status(500).send({
 				message: err.message || "An error has occurred."
 			})
 		} else {
-			// array of rows are already in the format of "data": []
-			res.json(rows) 
+			res.json(result)
 		}
 	})
 })
+
+/*  Retrieve eligible courses + details FOR engineer='keithchiang.2019@aio.com'
+    Eligible means:
+        (1) Met prereq AND
+        (2) Not currently taking AND
+        (3) Have not completed */
+router.get("/getIneligible", (req, res) => {
+    let sql = `
+        SELECT
+            course.course_id, course.course_name, course.course_summary, class.class_id, class.trainer_email, class.size, class.enrolment_start, class.enrolment_end, class.course_start, class.course_end, cpr.prereq_course_id
+        FROM
+            courses course, classes class, course_prereq cpr
+        WHERE
+            course.course_id = class.course_id
+        AND
+            class.is_published = 1
+        AND
+            class.material_status = 1
+        AND
+            class.course_id = cpr.course_id
+        AND
+            cpr.prereq_course_id not in (SELECT course_id from completed_courses WHERE engineer_email = 'keithchiang.2019@aio.com');
+        AND
+            (class.course_id, class.class_id) not in (SELECT course_id, class_id FROM enrolled WHERE engineer_email = 'keithchiang.2019@aio.com')
+        `
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "An error has occurred."
+            })
+        } else {
+            res.json(result)
+        }
+    })
+})
+
+// Retrieve ineligible courses + details FOR engineer='keithchiang.2019@aio.com'
+// Reason: Ineligible BECAUSE prerequisites not met
+router.get("/getIneligiblePrereq", (req, res) => {
+	let sql = `
+        SELECT
+            course.course_id, course.course_name, course.course_summary, class.class_id, t.name, class.trainer_email, class.size, class.enrolment_start, class.enrolment_end, class.course_start, class.course_end, cpr.prereq_course_id
+        FROM
+            courses course, classes class, course_prereq cpr, trainers t
+        WHERE
+            course.course_id = class.course_id
+        AND
+            class.is_published = 1
+        AND
+            class.material_status = 1
+        AND
+            class.course_id = cpr.course_id
+        AND
+            t.email = class.trainer_email
+        AND 
+            cpr.prereq_course_id not in (SELECT course_id from completed_courses WHERE engineer_email = 'keithchiang.2019@aio.com');
+    `
+
+	db.query(sql, (err, result) => {
+		if (err) {
+			res.status(500).send({
+				message: err.message || "An error has occurred."
+			})
+		} else {
+			res.json(result)
+		}
+	})
+})
+
+// Retrieve ineligible courses + details FOR engineer='keithchiang.2019@aio.com'
+// Reason: Ineligible BECAUSE currently enrolled
+router.get("/getIneligibleEnrolled", (req, res) => {
+	let sql = `
+        SELECT
+            course.course_id, course.course_name, course.course_summary, class.class_id, t.name, class.trainer_email, class.size, class.enrolment_start, class.enrolment_end, class.course_start, class.course_end, cpr.prereq_course_id
+        FROM
+            courses course, classes class, course_prereq cpr, trainers t
+        WHERE
+            course.course_id = class.course_id
+        AND
+            class.is_published = 1
+        AND
+            class.material_status = 1
+        AND
+            class.course_id = cpr.course_id
+        AND
+            t.email = class.trainer_email
+        AND 
+            (class.course_id, class.class_id) in (SELECT course_id, class_id FROM enrolled WHERE engineer_email = 'keithchiang.2019@aio.com');
+    `
+
+	db.query(sql, (err, result) => {
+		if (err) {
+			res.status(500).send({
+				message: err.message || "An error has occurred."
+			})
+		} else {
+			res.json(result)
+		}
+	})
+})
+
+// Retrieve ineligible courses + details FOR engineer='keithchiang.2019@aio.com'
+// Reason: Ineligible BECAUSE has completed it
+router.get("/getIneligibleCompleted", (req, res) => {
+	let sql = `
+        SELECT
+            course.course_id, course.course_name, course.course_summary, class.class_id, t.name, class.trainer_email, class.size, class.enrolment_start, class.enrolment_end, class.course_start, class.course_end
+        FROM
+            courses course, classes class, trainers t
+        WHERE
+            course.course_id = class.course_id
+        AND
+            class.is_published = 1
+        AND
+            class.material_status = 1
+        AND
+            t.email = class.trainer_email
+        AND 
+            class.course_id in (SELECT course_id from completed_courses WHERE engineer_email = 'keithchiang.2019@aio.com');
+    `
+
+	db.query(sql, (err, result) => {
+		if (err) {
+			res.status(500).send({
+				message: err.message || "An error has occurred."
+			})
+		} else {
+			res.json(result)
+		}
+	})
+})
+
+
 
 module.exports = router
--- sql/db.sql
+++ sql/db.sql
@@ -1,28 +1,215 @@
-select * from courses;
-select * from trainers;
-drop table courses;
+DROP DATABASE IF EXISTS spmg6t8;
+CREATE DATABASE spmg6t8;
+USE spmg6t8;
 
+################################################################################################################
+
+DROP TABLE IF EXISTS courses;
 CREATE TABLE courses (
-	courseID varchar(255),
-	courseName varchar(255),
-    class varchar(255),
-    size int,
-    trainer varchar(255),
-    startDate varchar(255),
-    endDate varchar(255)
+    course_id VARCHAR(255),
+    course_name VARCHAR(255),
+    course_summary VARCHAR(1000),
+    has_prereq BOOLEAN DEFAULT False,
+    
+    PRIMARY KEY (course_id)
 );
 
-INSERT INTO courses VALUES("CG1000C1", "Intro to Canon G1000", "C1", 24, "24/09/2021", "08/10/2021");
-INSERT INTO courses VALUES("CG1000C2", "Intro to Canon G1000", "C2", 20, "24/09/2021", "08/10/2021");
-INSERT INTO courses VALUES("CG2000C1", "Intro to Canon G2000", "C1", 30, "01/10/2021", "08/10/2021");
+INSERT INTO courses VALUES("CG1000", "Intro to Canon G1000", "Lorem Ipsum", False);
+INSERT INTO courses VALUES("CG2000", "Intro to Canon G2000", "Lorem Ipsum", True);
+INSERT INTO courses VALUES("CG3000", "Intro Canon G3000", "Lorem Ipsum", True);
+INSERT INTO courses VALUES("XK1000", "Intro to Xerox K1000", "Lorem Ipsum", False);
+INSERT INTO courses VALUES ("BG1000", "Intro to Brother G1000", "Lorem Ipsum", False);
+INSERT INTO courses VALUES ("BG2000", "Intermediate Brother G1000", "Lorem Ipsum", True);
+INSERT INTO courses VALUES ("BG3000", "Advanced Brother G1000", "Lorem Ipsum", True);
 
+################################################################################################################
+
+DROP TABLE IF EXISTS trainers;
 CREATE TABLE trainers (
-	name varchar(255),
     email varchar(255),
     password varchar(255),
-    qualified varchar(255)
+	name varchar(255),
+    qualified varchar(255),
+    
+    PRIMARY KEY (email)
 );
 
-INSERT INTO trainers VALUES("Larry Haverkamp", "lh.2021@aio.com", "lh", "CG1000");
-INSERT INTO trainers VALUES("Jack Sparrow", "js.2021@aio.com", "js", "CG2000");
-INSERT INTO trainers VALUES("Marry Littlelamb", "ml.2021@aio.com", "ml", "CG2000");
+INSERT INTO trainers VALUES("johnappleseed.2021@aio.com", "John Appleseed", "ja", "CG1000");
+INSERT INTO trainers VALUES("jacksparrow.2021@aio.com", "Jack Sparrow", "js", "CG2000");
+INSERT INTO trainers VALUES("janedoe.2021@aio.com", "Jane Doe", "jd", "CG2000");
+
+################################################################################################################
+
+DROP TABLE IF EXISTS engineers;
+CREATE TABLE engineers (
+    email varchar(255),
+    password varchar(255),
+	name varchar(255),
+    
+    PRIMARY KEY (email)
+);
+
+INSERT INTO engineers VALUES("keithchiang.2019@aio.com", "Keith Chiang", "kc");
+INSERT INTO engineers VALUES("htwong.2019@aio.com", "Jonathan Wong", "jw");
+INSERT INTO engineers VALUES("krystenng.2019@aio.com", "Krysten Ng", "kn");
+INSERT INTO engineers VALUES("hqyuen.2019@aio.com", "Yuen Huiqi", "hq");
+
+################################################################################################################
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+    course_id VARCHAR(255),
+    class_id VARCHAR(255),
+    trainer_email VARCHAR(255),
+    size INT,
+    current_enrolled INT,
+    enrolment_start VARCHAR(255),
+    enrolment_end VARCHAR(255),
+    course_start VARCHAR(255),
+    course_end VARCHAR(255),
+    material_status BOOLEAN DEFAULT False,
+    is_published BOOLEAN DEFAULT False,
+    
+    CONSTRAINT classes_pk PRIMARY KEY (course_id, class_id),
+    CONSTRAINT classes_fk1 FOREIGN KEY (trainer_email) REFERENCES trainers(email)
+);
+
+# populating data for johnappleseed.2021@aio.com
+INSERT INTO classes VALUES("CG1000", "C1", "johnappleseed.2021@aio.com", 10, 0, "10/09/2021", "20/09/2021", "24/09/2021", "30/11/2021", False, False);
+INSERT INTO classes VALUES("CG1000", "C2", "johnappleseed.2021@aio.com", 10, 0, "10/09/2021", "20/09/2021", "28/09/2021", "13/11/2021", True, False);
+INSERT INTO classes VALUES("BG1000", "C1", "johnappleseed.2021@aio.com", 10, 0, "10/09/2021", "20/09/2021", "02/10/2021", "23/12/2021", False, False);
+INSERT INTO classes VALUES("BG1000", "C2", "johnappleseed.2021@aio.com", 10, 0, "10/09/2021", "20/09/2021", "08/11/2021", "28/12/2021", False, False);
+INSERT INTO classes VALUES("XK1000", "C1", "johnappleseed.2021@aio.com", 10, 0, "10/09/2021", "20/09/2021", "17/10/2021", "29/12/2021", False, False);
+INSERT INTO classes VALUES("XK1000", "C2", "johnappleseed.2021@aio.com", 10, 0, "10/09/2021", "20/09/2021", "13/11/2021", "27/11/2021", False, False);
+INSERT INTO classes VALUES("XK1000", "C3", "johnappleseed.2021@aio.com", 10, 0, "10/09/2021", "20/09/2021", "14/09/2021", "25/12/2021", False, False);
+
+INSERT INTO classes VALUES("CG2000", "C1", "jacksparrow.2021@aio.com", 10, 0, "20/09/2021", "30/09/2021", "01/10/2021", "08/10/2021", True, False);
+INSERT INTO classes VALUES ("CG2000", "C2", "jacksparrow.2021@aio.com", 10, 0, "20/09/2021", "30/09/2021", "01/10/2021", "08/10/2021", True, True);
+
+# keith-newTestCases
+INSERT INTO classes VALUES("BG1000", "C3", "janedoe.2021@aio.com", 10, 0, "20/09/2021", "30/09/2021", "01/10/2021", "08/10/2021", True, True); # keith-completed
+INSERT INTO classes VALUES ("BG2000", "C1", "janedoe.2021@aio.com", 10, 0, "01/09/2021", "15/09/2021", "16/09/2021", "20/10/2021", True, True); # keith-ongoing
+INSERT INTO classes VALUES ("BG3000", "C1", "janedoe.2021@aio.com", 10, 0, "01/09/2021", "15/09/2021", "16/09/2021", "20/10/2021", True, True); # keith-ineligibleToTake
+INSERT INTO classes VALUES ("CG3000", "C1", "janedoe.2021@aio.com", 10, 0, "01/09/2021", "15/09/2021", "16/09/2021", "20/10/2021", True, True); # keith-ongoing
+INSERT INTO classes VALUES("XK1000", "C4", "janedoe.2021@aio.com", 10, 0, "12/10/2021", "20/10/2021", "21/10/2021", "21/11/2021", True, True); # keith-toEnroll
+
+################################################################################################################
+
+DROP TABLE IF EXISTS completed_courses;
+CREATE TABLE completed_courses (
+    engineer_email varchar(255),
+    course_id VARCHAR(255),
+    class_id VARCHAR(255),
+    completed_date VARCHAR(255),
+    
+    CONSTRAINT completed_courses_pk PRIMARY KEY (engineer_email, course_id, class_id),
+    CONSTRAINT completed_courses_fk1 FOREIGN KEY (engineer_email) REFERENCES engineers(email),
+    CONSTRAINT completed_courses_fk2 FOREIGN KEY (course_id, class_id) REFERENCES classes(course_id, class_id)
+);
+
+INSERT INTO completed_courses VALUES("htwong.2019@aio.com", "CG1000", "C1", "08/10/2021");
+
+INSERT INTO completed_courses VALUES("keithchiang.2019@aio.com", "CG1000", "C1", "08/10/2021");
+INSERT INTO completed_courses VALUES("keithchiang.2019@aio.com", "CG2000", "C2", "08/10/2021");
+INSERT INTO completed_courses VALUES("keithchiang.2019@aio.com", "BG1000", "C3", "08/10/2021");
+
+################################################################################################################
+
+DROP TABLE IF EXISTS course_prereq;
+CREATE TABLE course_prereq (
+    course_id VARCHAR(255),
+    prereq_course_id VARCHAR(255),
+
+    CONSTRAINT prereq_pk PRIMARY KEY (course_id, prereq_course_id),
+    CONSTRAINT prereq_pk1 FOREIGN KEY (course_id) REFERENCES courses(course_id),
+    CONSTRAINT prereq_pk2 FOREIGN KEY (prereq_course_id) REFERENCES courses(course_id)
+);
+
+INSERT INTO course_prereq VALUES("CG2000", "CG1000");
+
+INSERT INTO course_prereq VALUES("CG3000", "CG1000");
+INSERT INTO course_prereq VALUES("CG3000", "CG2000");
+
+INSERT INTO course_prereq VALUES("BG2000", "BG1000");
+INSERT INTO course_prereq VALUES("BG3000", "BG2000");
+
+################################################################################################################
+
+DROP TABLE IF EXISTS enrolled;
+CREATE TABLE enrolled (
+    engineer_email VARCHAR(255),
+    course_id VARCHAR(255),
+    class_id VARCHAR(255),
+    
+    CONSTRAINT enrolled_pk PRIMARY KEY (engineer_email, course_id, class_id),
+    CONSTRAINT enrolled_fk1 FOREIGN KEY (engineer_email) REFERENCES engineers(email),
+    CONSTRAINT enrolled_fk2 FOREIGN KEY (course_id, class_id) REFERENCES classes(course_id, class_id)
+);
+
+INSERT INTO enrolled VALUES("keithchiang.2019@aio.com", "BG2000", "C1");
+
+################################################################################################################
+
+DROP TABLE IF EXISTS enrol_request;
+CREATE TABLE enrol_request (
+    engineer_email VARCHAR(255),
+    course_id VARCHAR(255),
+    class_id VARCHAR(255),
+    
+    CONSTRAINT enrol_request_pk PRIMARY KEY (engineer_email, course_id, class_id),
+    CONSTRAINT enrol_request_fk1 FOREIGN KEY (engineer_email) REFERENCES engineers(email),
+    CONSTRAINT enrol_request_fk2 FOREIGN KEY (course_id, class_id) REFERENCES classes(course_id, class_id)
+);
+
+################################################################################################################
+
+DROP TABLE IF EXISTS course_materials;
+CREATE TABLE course_materials (
+    course_id VARCHAR(255),
+    class_id VARCHAR(255),
+    chapter_id VARCHAR(255),
+    content VARCHAR(255),
+    quiz_id VARCHAR(255),
+    
+    CONSTRAINT course_materials_pk PRIMARY KEY (course_id, class_id, chapter_id),
+    CONSTRAINT course_materials_fk1 FOREIGN KEY (course_id, class_id) REFERENCES classes(course_id, class_id)
+    # CONSTRAINT course_materials_fk2 FOREIGN KEY (quiz_id) REFERENCES quizzes(quiz_id)
+);
+
+INSERT INTO course_materials VALUES("CG1000", "C1", "123", "Dummy Content", "1");
+
+################################################################################################################
+
+DROP TABLE IF EXISTS quizzes;
+CREATE TABLE quizzes (
+    quiz_id VARCHAR(64),
+    course_id VARCHAR(64),
+    class_id VARCHAR(64),
+    chapter_id VARCHAR(64),
+    has_questions BOOLEAN,
+    
+    CONSTRAINT quizzes_pk PRIMARY KEY (quiz_id, course_id, class_id, chapter_id),
+    CONSTRAINT quizzes_fk1 FOREIGN KEY (course_id, class_id) REFERENCES classes(course_id, class_id)
+    # CONSTRAINT quizzes_fk2 FOREIGN KEY (chapter_id) REFERENCES course_materials(chapter_id)
+);
+
+################################################################################################################
+
+DROP TABLE IF EXISTS quiz_questions;
+CREATE TABLE quiz_questions (
+    quiz_id VARCHAR(255),
+    question_id VARCHAR(255),
+    question VARCHAR(255),
+    type VARCHAR(255),
+    duration INT,
+    option1 VARCHAR(255),
+    option2 VARCHAR(255),
+    option3 VARCHAR(255),
+    option4 VARCHAR(255),
+    answer VARCHAR(255),
+    
+    CONSTRAINT quiz_questions_pk PRIMARY KEY (quiz_id, question_id),
+    CONSTRAINT quiz_questions_fk1 FOREIGN KEY (quiz_id) REFERENCES quizzes(quiz_id)
+);
+
+################################################################################################################
