074f0e8109339d64712034296a8c0c8619902b08
yuenhuiqi <huiqi.yuen.2019@scis.smu.edu.sg>
2021-10-27 23:04:18 +0800

    [G6T8-60] Trainer - Upload materials for courses

--- backend/routes/chapters.js
+++ backend/routes/chapters.js
@@ -4,7 +4,7 @@ const db = require("../db")
 
 // get all course materials
 router.get("/", (req, res) => {
-	let sql = "select * from course_materials"
+	let sql = "select * from teaching_materials"
 
 	db.query(sql, (err, rows) => {
 		if (err) {
@@ -19,12 +19,11 @@ router.get("/", (req, res) => {
 
 // post new chapter to db
 router.post("/addChapter", (req, res) => {
-	let sql = `INSERT INTO course_materials VALUES ( \
+	let sql = `INSERT INTO teaching_materials VALUES ( \
 		"${req.body.courseID}", \
 		"${req.body.classID}", \
-		"${req.body.chapNum}", \
-        "${req.body.content}", \
-		${req.body.quizID}
+		"${req.body.chapterID}", \
+        "${req.body.content}"
 	)`
 
 	db.query(sql, (err, result) => {
@@ -36,10 +35,29 @@ router.post("/addChapter", (req, res) => {
             console.log(sql)
 		} else {
 			res.status(200).send({
-				message: "1 record inserted to course_materials table"
+				message: "1 record inserted to teaching_materials table"
 			})
 		}
 	})
 })
 
+// Get all course materials by course ID and class ID
+router.get("/getAllChapters", (req, res) => {
+	let courseID = req.query.course_id
+	let classID = req.query.class_id
+
+	let sql = `SELECT * from teaching_materials WHERE 
+	course_id="${courseID}" and class_id="${classID}"`
+
+	db.query(sql, (err, rows) => {
+		if (err) {
+			res.status(500).send({
+				message: err.message || "An error has occured."
+			})
+			console.log("hel")
+		} else {
+			res.json(rows) 
+		}
+	})
+})
 module.exports = router
--- frontend/src/pages/Trainer/TrainerManageCourse.js
+++ frontend/src/pages/Trainer/TrainerManageCourse.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import TrainerChapter from "../../components/TrainerChapter";
 import { Container, Button } from 'react-bootstrap'
 import { Link, useRouteMatch, useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styled from "styled-components";
 import axios from 'axios';
 
@@ -15,37 +15,59 @@ const ChapterDiv = styled.div`
     margin-bottom: 20px;
 `
 
+const ExistingChapterDiv = styled.div`
+    margin-bottom: 20px;
+`
+
 const TrainerManageCourse = () => {
-    const { url } = useRouteMatch()
+    // const { url } = useRouteMatch()
     const { courseID, classID } = useParams()
+
+    const [chapters, setChapters] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:5000/chapters/getAllChapters?course_id=${courseID}&class_id=${classID}`)
+            .then(res => {
+                setChapters(res.data)
+            })
+    }, [courseID, classID])
+
+    // console.log(Object.entries(chapters))
     const [allChaps, setAllChaps] = useState([])
     const [chapNum, setChapNum] = useState(1)
+    // console.log(allChaps)
+    console.log(allChaps)
+    console.log(chapters.length)
+
+    // // only if there's no chapters at all
+    // const [allChaps, setAllChaps] = useState([])
+    // const [chapNum, setChapNum] = useState(1)
 
     const addChapter = e => {
+        // console.log(chapters.length)
+        // console.log(chapNum)
+        // setChapNum(chapNum + chapters.length)
+        setAllChaps( allChaps => [...allChaps, <TrainerChapter key={chapNum} count={chapNum} />])
+        setChapNum(chapNum + 1)
         e.preventDefault()
-        let content = 'Replace content here'
-        let quizID = "quiz" + chapNum
+        console.log(chapNum)
         axios.post("http://127.0.0.1:5000/chapters/addChapter", {
             "courseID": courseID, 
             "classID": classID, 
             "chapterID": chapNum, 
-            "content": content,
-            "quiz_id": quizID
+            "content": "",
         })
-        setAllChaps( allChaps => [...allChaps, <TrainerChapter key={chapNum} count={chapNum} />])
-        setChapNum(chapNum + 1)
-        console.log(chapNum)
     }
 
     return (
         <Container>
-            <Header>Course: {courseID}</Header> 
+            <Header>Course: {courseID}</Header>
             <Button onClick={addChapter}> Add Chapter </Button>
+            <ExistingChapterDiv><h1> Hi </h1></ExistingChapterDiv>
             <ChapterDiv className="allChaps">
                 {allChaps.map((a, i) => (a))}
             </ChapterDiv>
         </Container>
-
     )
 }
 
