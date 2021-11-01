0eb35835e33503dee01c9b74bd991708cce748be
jonjonnyjonjon <70429099+jonjonnyjonjon@users.noreply.github.com>
2021-11-01 21:21:33 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,9 @@ app.use(express.json())
 
 // Routes for Heroku
 app.use(express.static(path.join(__dirname, "..", "build")))
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, "..", 'build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, "..", 'build', 'index.html'));
+// });
 
 // Import routes
 const coursesRoute = require("./routes/courses")
