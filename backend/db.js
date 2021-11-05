22d37ade8a1897fbeb37ab811f13469c5d9fa4bb
jonjonnyjonjon <70429099+jonjonnyjonjon@users.noreply.github.com>
2021-11-05 13:28:58 +0800

    Changed db.js to use the correct env variables

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ const mysql = require("mysql");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: process.env.NODE_ENV === "production" ? process.env.RDS_HOSTNAME : process.env.DB_HOST,
+    user: process.env.NODE_ENV === "production" ? process.env.RDS_USERNAME : process.env.DB_USER,
+    password: process.env.NODE_ENV === "production" ? process.env.RDS_PASSWORD : process.env.DB_PASSWORD,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DB_NAME
 })
 
 // open the MySQL connection
